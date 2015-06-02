0aadda6dcdd33051befd9565be32b76362523dca
Parmeet Singh <parmeetmander90@gmail.COM>
2015-06-02 18:00:03 -0400

    added backbone and handlebars gem, added backbone folders and templates folder

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "registrations" }
 
   get "/new_profile", to: "profiles#new"
+  get "/show", to: "profiles#show"
+  post '/create', to: "profiles#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
