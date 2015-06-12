4d6d944b414ee2ac2fc8dc736a58d371b31104bf
Parmeet Singh <parmeetmander90@gmail.COM>
2015-06-12 18:28:16 -0400

    changed ajax urls to my own api controller to bypass the https heroku requirements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -46,15 +46,13 @@ var ready = function loadStocksApp(){
 				findStock = function(){
 					$.ajax({
 					type: 'GET',
-					dataType: 'jsonp',
-					jsonp: 'callback',
-					url: 'http://dev.markitondemand.com/Api/v2/Lookup/jsonp?input=' + newCompany.val()
+					dataType: 'json',
+					url: '/partysymbol?company=' + newCompany.val()
 				}).done(function(data){
 					$.ajax({
 						type: 'GET',
-						dataType: 'jsonp',
-						jsonp: 'callback',
-						url: 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=' + data[0].Symbol
+						dataType: 'json',
+						url: '/partystock?symbol=' + data[0].Symbol
 					}).done(function(data1){
 						collection.create({name: data1.Name, symbol: data1.Symbol, lastprice: data1.LastPrice, open: data1.Open, high: data1.High, low: data1.Low});
 					})
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,13 +2,13 @@ class ApiController < ApplicationController
 
 	def symbol
 		@company = params[:company]
-		@symbol = HTTParty.get("http://dev.markitondemand.com/Api/v2/Lookup/jsonp?input=#{@company}&callback=stock")
+		@symbol = HTTParty.get("http://dev.markitondemand.com/Api/v2/Lookup/json?input=#{@company}&callback=stock")
 		render json: @symbol.body
 	end
 
 	def stock
 		@symbol = params[:symbol]
-		@stock = HTTParty.get("http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=#{@symbol}&callback=stock")
+		@stock = HTTParty.get("http://dev.markitondemand.com/Api/v2/Quote/json?symbol=#{@symbol}&callback=stock")
 		render json: @stock.body
 	end
 
